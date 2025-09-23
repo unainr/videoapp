737c95f6df77c73f291428f6bbd87a674073acd9
UNAINR <munain605@gmail.com>
2025-09-23 11:06:07 +0500

    auth-fixed

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,15 @@
-import { SignUpForm } from '@/components/auth/sign-up/SignUpForm'
 import React from 'react'
-
-const SignUpPage = () => {
+import { SignUpForm } from '@/components/auth/sign-up/SignUpForm'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+const SignUpPage = async () => {
+   const session = await auth.api.getSession({
+      headers: await headers(),
+    })
+      if(!!session){
+      redirect("/")
+    }
   return (
     <><SignUpForm/></>
   )
