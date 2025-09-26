8d9a50e9b5005996db7eafc13babba03168e982e
UNAINR <munain605@gmail.com>
2025-09-26 11:15:20 +0500

    add-data-table

--- modules/agents/ui/views/agents-view.tsx
+++ modules/agents/ui/views/agents-view.tsx
@@ -4,13 +4,17 @@ import {  useSuspenseQuery } from '@tanstack/react-query'
 import { useTRPC } from '@/trpc/client'
 import LoadingState from '@/components/loading/loading-state'
 import ErrorState from '@/components/loading/error-state'
+import { DataTable } from './components/data-table'
+import { columns } from './components/columns'
+
+
 export const AgentsView = () => {
   const trpc = useTRPC();
   const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions())
 
     return (
-    <div className='flex flex-col items-center justify-center min-h-screen max-w-4xl'>
-      {JSON.stringify(data,null,2)}
+    <div className='flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-5'>
+     <DataTable data={data} columns={columns}/>
     </div>
   )
 }
---
+++ modules/agents/ui/views/components/columns.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Badge } from "@/components/ui/badge";
+import { AgentGetOne } from "@/modules/agents/types";
+import { ColumnDef } from "@tanstack/react-table";
+import { CornerDownRightIcon, VideoIcon } from "lucide-react";
+
+export const columns: ColumnDef<AgentGetOne>[] = [
+	{
+		accessorKey: "name",
+		header: "Agent Name",
+		cell: ({ row }) => (
+			<div className="flex flex-col gap-y-2">
+				<div className="flex items-start gap-x-2">
+					{/* Avatar */}
+					<GeneratedAvatar
+						variant="botttsNeutral"
+						seed={row.original.name}
+						className="size-6 shrink-0"
+					/>
+
+					{/* Content */}
+					<div className="flex flex-col">
+						{/* Name */}
+						<span className="font-semibold capitalize">
+							{row.original.name}
+						</span>
+
+						{/* Instructions with icon */}
+						<div className="flex items-center gap-x-2 text-muted-foreground">
+							<CornerDownRightIcon className="size-3 shrink-0" />
+							<span className="text-sm max-w-[200px] truncate capitalize">
+								{row.original.instructions}
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		),
+	},
+	{
+		accessorKey: "meetingCount",
+		header: "Meetings",
+		cell: ({ row }) => (
+			<Badge
+				variant={"outline"}
+				className="flex items-center gap-x-2 [&>svg]:size-4">
+				<VideoIcon className="text-blue-600" />5 meetings
+			</Badge>
+		),
+	},
+];
