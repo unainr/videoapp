7358219f5108e926ab63471ec618034c72c54e08
UNAINR <munain605@gmail.com>
2025-10-02 22:19:06 +0500

    agent-update

--- modules/agents/ui/views/agent-id-view.tsx
+++ modules/agents/ui/views/agent-id-view.tsx
@@ -2,26 +2,61 @@
 import ErrorState from '@/components/loading/error-state'
 import LoadingState from '@/components/loading/loading-state'
 import { useTRPC } from '@/trpc/client'
-import { useSuspenseQuery } from '@tanstack/react-query'
-import React from 'react'
+import { useMutation, useQueryClient, useSuspenseQuery } from '@tanstack/react-query'
+import React, { useState } from 'react'
 import { AgentIdViewHeader } from './components/agent-id-view-header'
 import { GeneratedAvatar } from '@/components/generated-avatar'
 import { Badge } from '@/components/ui/badge'
 import { VideoIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+import { useConfirm } from '../../hooks/use-confirm'
+import { UpdateAgentDialog } from './components/update-agent-dialog'
 
 interface AgentIdViewProps{
     agentId: string
 }
 export const AgentIdView = ({ agentId }: AgentIdViewProps) => {
+    const router = useRouter()
+    const queryClient = useQueryClient()
     const trpc = useTRPC()
+    const [updateAgentDialogOpen, setupdateAgentDialogOpen] = useState(false)
     const {data} = useSuspenseQuery(trpc.agents.getOne.queryOptions({id:agentId}))
+     const removeAgent = useMutation(
+        trpc.agents.remove.mutationOptions({
+            onSuccess: async()=>{
+                await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}))
+                router.push('/agents')
+            },
+            onError:(error)=>{
+                toast.error(error.message)
+            }
+        })
+    )
+    const [RemoveConfirmation,confirmRemove]= useConfirm(
+        'Are Your sure?',
+        `The following action will action ${data.meetingCount} associted meetings`
+    )
+    const handleRemoveAgent = async()=>{
+        const ok = await confirmRemove();
+        if(!ok)return
+        await removeAgent.mutateAsync({id:agentId})
+    }
     return (
+        <>
+        <RemoveConfirmation/>
+        <UpdateAgentDialog
+        open={updateAgentDialogOpen}
+        onOpenChange={setupdateAgentDialogOpen}
+        initialValues={data}
+        
+        />
 <div className="flex-1 pt-16 py-4 px-4 md:px-8 flex flex-col gap-y-4">
            <AgentIdViewHeader
             agentId={agentId}
             agentName={data.name}
-            onEdit={()=>{}}
-            onRemove={()=>{}}
+            onEdit={()=>setupdateAgentDialogOpen(true)}
+            onRemove={handleRemoveAgent}
            />
            <div className='bg-white rounded-lg border'>
                 <div className='px-4 py-5 gap-y-5 flex flex-col col-span-5'>
@@ -46,6 +81,7 @@ export const AgentIdView = ({ agentId }: AgentIdViewProps) => {
                 </div>
            </div>
         </div>
+        </>
     )
 }
 
--- modules/agents/ui/views/components/agent-form.tsx
+++ modules/agents/ui/views/components/agent-form.tsx
@@ -37,6 +37,21 @@ export const AgentForm = ({
                await queryClient.invalidateQueries(
                     trpc.agents.getMany.queryOptions({}),
                 )
+               
+                onSuccess?.()
+            },
+			onError: (error) => {
+                toast.error(error.message)
+            },
+		})
+	);
+
+const updateAgent = useMutation(
+		trpc.agents.update.mutationOptions({
+			onSuccess: async () => {
+               await queryClient.invalidateQueries(
+                    trpc.agents.getMany.queryOptions({}),
+                )
                 if(initialValues?.id){
                    await queryClient.invalidateQueries(
                         trpc.agents.getOne.queryOptions({id:initialValues.id})
@@ -50,6 +65,7 @@ export const AgentForm = ({
 		})
 	);
 
+
 	const form = useForm<z.infer<typeof agentsInsertSchema>>({
 		resolver: zodResolver(agentsInsertSchema),
 		defaultValues: {
@@ -59,11 +75,11 @@ export const AgentForm = ({
 	});
 
 	const isEdit = !!initialValues?.id;
-	const isPending = createAgent.isPending;
+	const isPending = createAgent.isPending || updateAgent.isPending;
 
 	const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
 		if (isEdit) {
-			console.log("TODO:UpdateAgent");
+			updateAgent.mutate({...values,id:initialValues.id});
 		} else {
 			createAgent.mutate(values);
 		}
