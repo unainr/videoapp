6e7f73239bdb724506c773a795fb48dddfe546f0
UNAINR <munain605@gmail.com>
2025-10-03 22:36:55 +0500

    meetings table

--- modules/agents/ui/views/components/columns.tsx
+++ modules/agents/ui/views/components/columns.tsx
@@ -2,11 +2,11 @@
 
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Badge } from "@/components/ui/badge";
-import { AgentGetOne } from "@/modules/agents/types";
+import { AgentGetOne, AgentsGetMany } from "@/modules/agents/types";
 import { ColumnDef } from "@tanstack/react-table";
 import { CornerDownRightIcon, VideoIcon } from "lucide-react";
 
-export const columns: ColumnDef<AgentGetOne>[] = [
+export const columns: ColumnDef<AgentsGetMany[number]>[] = [
 	{
 		accessorKey: "name",
 		header: "Agent Name",
--- modules/meetings/ui/views/meeting-view.tsx
+++ modules/meetings/ui/views/meeting-view.tsx
@@ -1,16 +1,18 @@
 'use client'
+import { DataTableMain } from '@/components/data-table-main';
 import ErrorState from '@/components/loading/error-state';
 import LoadingState from '@/components/loading/loading-state';
 import { useTRPC } from '@/trpc/client';
 import {  useSuspenseQuery } from '@tanstack/react-query';
 import React from 'react'
+import { columnsmeeting } from '../components/columns';
 
 export const MeetingView = () => {
     const trpc = useTRPC();
     const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
   return (
-    <div className='w-full'>
-        {JSON.stringify(data,null,2)}
+<div className='flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-5'>        
+        <DataTableMain data={data.items} columns={columnsmeeting}/>
     </div>
   )
 }
