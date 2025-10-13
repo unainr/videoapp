ea607314b47baa412fc049252061cc846b0369a9
UNAINR <munain605@gmail.com>
2025-10-13 21:48:43 +0500

    meetings varients

---
+++ modules/meetings/ui/components/cancelled-state.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const CancelledState = () => {
+    return (
+        <div className="bg-white rounded-lg px-4 py-5 flex flex-col gap-y-8 items-center justify-center">
+            
+        Cancel meeting
+            
+        </div>
+    );
+};
+
+export default CancelledState;
